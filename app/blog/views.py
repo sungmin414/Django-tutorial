46f3aa60a47ef00f654ab759f10bcc087b9fe0c0
sungmin414 <sungmin3231@gmail.com>
2018-06-06 19:55:58 +0900

    render_to_string대신에 render를 사용

--- app/blog/views.py
+++ app/blog/views.py
@@ -15,6 +15,9 @@ def post_list(request):
     # html = open(post_list_template_path,'rt').read()
 
     # 경로에 해당하는 HTML파일을 문자열로 로드해줌
-    html = render_to_string('blog/post_list.html')
-    # 가져온 문자열을 돌려주기
-    return HttpResponse(html)
+    # html = render_to_string('blog/post_list.html')
+    # # 가져온 문자열을 돌려주
+    # return HttpResponse(html)
+
+    # 위 명령어 줄여쓴 말
+    return render(request,'blog/post_list.html')
